5739a8c5428ce60b005106a889f72f7421ac569e
Abram Elton Ntsako Nyathi <89238406+AEN-Nyathi@users.noreply.github.com>
2026-01-18 14:35:57 +0200

    Refactor createClient function to remove cookieStore parameter

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -5,7 +5,9 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 
-export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+// export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+    
+export const createClient = () => {
     if(!supabaseUrl || !supabaseAnonKey){
     return new error("no: supabaseUrl or supabaseAnonKey")
   }
