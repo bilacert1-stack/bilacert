45db6d1b3220c33c804b7ebeb45be9d794800a10
Abram Elton Ntsako Nyathi <89238406+AEN-Nyathi@users.noreply.github.com>
2026-01-18 17:38:07 +0200

    Fix error instantiation in createClient function

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -6,7 +6,7 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export function createClient() {
   if(!supabaseUrl || !supabaseAnonKey){
-    return new error("no: supabaseUrl or supabaseAnonKey")
+    return new Error("no: supabaseUrl or supabaseAnonKey")
   }
     return createBrowserClient(
         supabaseUrl!, supabaseAnonKey!
