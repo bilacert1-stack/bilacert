bdbd8fb358931ecff5d1adeba31c2fcea04b2afa
Abram Elton Ntsako Nyathi <89238406+AEN-Nyathi@users.noreply.github.com>
2026-01-18 14:26:40 +0200

    Refactor createClient to use cookieStore
    Refactor createClient function to accept cookieStore and handle cookies for Supabase client creation.

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,13 +1,32 @@
-import { createServerClient } from '@supabase/supabase-js'
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export function createClient() {
-  if(!supabaseUrl || !supabaseAnonKey){
+
+export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+    if(!supabaseUrl || !supabaseAnonKey){
     return new error("no: supabaseUrl or supabaseAnonKey")
   }
-    return createServerClient(
-        supabaseUrl!, supabaseAnonKey!
-    )
-}
+  return createServerClient(
+    supabaseUrl!,
+    supabaseKey!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    },
+  );
+};
